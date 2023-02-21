95a819e38ff4d6c93d7728dc59fb9e4e9fea9dec
Rob Lewis <rob.y.lewis@gmail.com>
2023-02-21 17:25:00 -0500

    bump mathlib version

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 open Lake DSL
 
 
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "724a444a35a78ce1922f5c3626ff618f50976f62"
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "e7e49c8a41239716cb79fa187a6bf66e7d82d710"
 require autograder from git "https://github.com/robertylewis/cs22-lean-autograder" @ "1c6119111649e9c18594be3b3722836025a96e86"
 
 package «brown-cs22» {
